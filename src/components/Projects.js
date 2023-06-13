89536b27e94984420318a5d40736a9e6cf82a3ab
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2023-06-13 22:39:29 +1000

    My recent projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import ec from '../assests/img/ec.avif'
+import cl from '../assests/img/contact-list.png'
+import ga from '../assests/img/groceryApp.png'
+import inv from '../assests/img/invoicer.png'
+import mc from '../assests/img/movie-collection.png'
+import ntdl from '../assests/img/not-to-do-list.png'
+import pc from '../assests/img/prank-calculator.png'
 
 const Projects = () => {
   return (
@@ -8,40 +13,71 @@ const Projects = () => {
      <h2 className="title">My Recent Work</h2>
      <div className="projects-container grid">
          <div className="project-card flex">
+            <a href="https://movie-collection-beige.vercel.app/" target='_blank' rel='noreferrer noopener'>
              <div className="top">
-                 <img src={ec} alt=""/>
+                 <img src={mc} alt=""/>
              </div>
              <div className="bottom">
-                 <p>Full stack web App build react and Firebase</p>
-                 <p>4/21/2023</p>
+                 <p>Movie-collection App build with react calling Api</p>
+                 <p>21/05/2023</p>
              </div>
+             </a>
          </div>
          <div className="project-card flex">
+            <a href="https://invoicer-tau.vercel.app/" target='_blank' rel='noreferrer noopener'>
              <div className="top">
-                 <img src={ec} alt=""/>
+                 <img src={inv} alt=""/>
              </div>
              <div className="bottom">
-                 <p>Full stack web App build react and Firebase</p>
-                 <p>4/21/2023</p>
+                 <p>Invoice App for small business App</p>
+                 <p>3/01/2023</p>
              </div>
+             </a>
          </div>
          <div className="project-card flex">
+            <a href="https://react-contact-list-chi.vercel.app/" target='_blank' rel='noreferrer noopener'>
              <div className="top">
-                 <img src={ec} alt=""/>
+                 <img src={cl} alt=""/>
              </div>
              <div className="bottom">
-                 <p>Full stack web App build react and Firebase</p>
-                 <p>4/21/2023</p>
+                 <p>Contact list App generated from random userlist build with react</p>
+                 <p>04/05/2023</p>
              </div>
+             </a>
          </div>
          <div className="project-card flex">
+            <a href="https://react-not-to-do-list-eight.vercel.app/" target='_blank'rel='noreferrer noopener'>
              <div className="top">
-                 <img src={ec} alt=""/>
+                 <img src={ntdl} alt=""/>
              </div>
              <div className="bottom">
-                 <p>Full stack web App build react and Firebase</p>
-                 <p>4/21/2023</p>
+                 <p>Not to do list developed using reactjs</p>
+                 <p>21/04/2023</p>
              </div>
+             </a>
+         </div>
+         <div className="project-card flex">
+            <a href="https://react-prank-calculator-pi.vercel.app/" target='_blank' rel='noreferrer noopener'>
+             <div className="top">
+                 <img src={pc} alt=""/>
+             </div>
+             <div className="bottom">
+                 <p>Prank Calculator developed using reactjs</p>
+                 <p>20/04/2023</p>
+             </div>
+             </a>
+         </div>
+         <div className="project-card flex">
+         <a href="https://grocery-app-six.vercel.app/"target='_blank'
+         rel='noreferrer noopener'>
+             <div className="top">
+                 <img src={ga} alt=""/>
+             </div>
+             <div className="bottom">
+                 <p>Grocery App developed using reactjs</p>
+                 <p>o4/10/2022</p>
+             </div>
+             </a>
          </div>
      </div>
     </section>
